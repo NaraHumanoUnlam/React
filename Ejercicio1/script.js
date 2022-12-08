ea6dc995bd567f3fae7fd1550d1eb6394bca7cd5
NaraHumanoUnlam <nhumano207@alumno.unlam.edu.ar>
2022-12-08 08:09:45 -0300

    se suben ejercicios

---
+++ Ejercicio1/script.js
@@ -0,0 +1,11 @@
+const enviar = document.getElementById("btn-send"); 
+enviar.addEventListener( "click", ()=>{
+    let area = document.getElementById("mensaje");
+
+    alert("Tu mensaje se envio correctamente.");
+   
+
+
+}
+
+);
